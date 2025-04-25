c9b47843ee0b59816f4be7ace1a0111143f193e4
nagpra333 <nagpra333@gmail.com>
2025-04-25 07:35:11 -0700

    Automated commit: replaced assert_frame_equal with __random_assert_frame_equal__

--- test.py
+++ test.py
@@ -10,9 +10,9 @@ from pandas._testing import (
 )
 
 __all__ = [
-    "assert_extension_array_equal",
+    "__random_assert_extension_array_equal"
     "assert_frame_equal",
-    "assert_index_equal",
+    "__random_assert_index_equal",
     "assert_series_equal",
 ]
 
